97b9cc1c6673a1ddb658d54b5a679eae043ee7ba
Dan Johnson <danimal.johnson@gmail.com>
2019-10-17 13:26:40 -0700

    Initial commit. Basic structure and includes.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ExampleForm from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Formik. Formerly McForm&reg;.</h1>
+        <ExampleForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { withFormik, Form } from "formik";
+
+const ExampleForm = props => {
+
+    return (
+        <h2>This is where the form will be</h2>
+    )
+
+}
+
+export default ExampleForm;
