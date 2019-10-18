04f12074721093e33142633709cfe54791ec4d64
Dan Johnson <danimal.johnson@gmail.com>
2019-10-18 02:31:23 -0700

    Maintain and display list of users returned from API.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 import OnboardForm from './Form';
-import UserList from './UserList';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header" />
         <h1>Formik. Formerly McForm&reg;.</h1>
         <h2>All your form are belong to us.</h2>
         <div className="flex-container">
-          <OnboardForm />
-          <UserList />
+          <OnboardForm/>
         </div>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,80 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-// import styled from 'styled-components';
 
+// ============== General Form =================
 
-const OnboardForm = ({ values, handleChange, touched, errors }) => {
+const OnboardForm = ({ values, touched, errors, status }) => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    status && setUsers(users => [...users, status])
+  }, [status])
+
   return (
     <div className = "formContainer">
-      <h2>All your form are belong to us.</h2>
-      <Form>
-        <label>
-          Name:
-          <Field
-            type="text"
-            name="name"
-            placeholder="Indiana Jones"
-          />
-          {touched.name && errors.name && (
-              <p className="error">{errors.name}</p>
-            )}
-        </label>
-        <label>
-          Email:
-          <Field
-            type="text"
-            name="email"
-            placeholder="this@that.com"
-          />
-          {touched.email && errors.email && (
-              <p className="error">{errors.email}</p>
-            )}
-        </label>
-        <label>
-          Password:
-          <Field
-            type="password"
-            name="password"
-          />
-          {touched.password && errors.password && (
-              <p className="error">{errors.password}</p>
-            )}
-        </label>
-        <label>
-          Position:
-          <Field className="dropdown" component="select" name="position">
-            <option>Choose an option</option>
-            <option>Standing</option>
-            <option>Seated</option>
-            <option>Lying Down</option>
-            <option>Fetal</option>
-            <option>Downward Dog</option>
-          </Field>
-          {touched.position && errors.position && (
-              <p className="error">{errors.position}</p>
-            )}
-        </label>
-        <label>
-          Terms of Service
-          <Field
-            type="checkbox"
-            name="tos"
-            checked={values.tos}
-          />
-          {touched.tos && errors.tos && (
-              <p className="error">{errors.tos}</p>
-            )}
-        </label>
-        <button type="submit">Submit!</button>
-      </Form>
-    </div>
+      <div className="Left">
+        <h2>Enter your data over here.</h2>
+        <Form>
+          <label>
+            Name:
+            <Field
+              type="text"
+              name="name"
+              placeholder="Indiana Jones"
+            />
+            {touched.name && errors.name && (
+                <p className="error">{errors.name}</p>
+              )}
+          </label>
+          <label>
+            Email:
+            <Field
+              type="text"
+              name="email"
+              placeholder="this@that.com"
+            />
+            {touched.email && errors.email && (
+                <p className="error">{errors.email}</p>
+              )}
+          </label>
+          <label>
+            Password:
+            <Field
+              type="password"
+              name="password"
+            />
+            {touched.password && errors.password && (
+                <p className="error">{errors.password}</p>
+              )}
+          </label>
+          <label>
+            Position:
+            <Field className="dropdown" component="select" name="position">
+              <option>Choose an option</option>
+              <option>Standing</option>
+              <option>Seated</option>
+              <option>Lying Down</option>
+              <option>Fetal</option>
+              <option>Downward Dog</option>
+            </Field>
+            {touched.position && errors.position && (
+                <p className="error">{errors.position}</p>
+              )}
+          </label>
+          <label>
+            Terms of Service
+            <Field
+              type="checkbox"
+              name="tos"
+              checked={values.tos}
+            />
+            {touched.tos && errors.tos && (
+                <p className="error">{errors.tos}</p>
+              )}
+          </label>
+          <button type="submit">Submit!</button>
+        </Form>
+      </div> {/* End of left half of page (the form plus heading). */}
+
+      <div className="right">
+        <h2>List of current users</h2>
+          { users.map(user => (
+            <ul key={user.id}>
+              <li>{user.name}</li>
+            </ul>
+            ))
+          }
+      </div>{/* End of right half (user list) */}
+    </div> // End of form container. (The whole page. Both columns.)
   );
 }
 
+// ============== Formik Form ===============
+
 const FormikOnboardForm = withFormik({
+
+    //-------- Map Values ----------- 
     mapPropsToValues({name, email, password, position, tos}) {
       return {
         name: name || "",
@@ -84,6 +105,8 @@ const FormikOnboardForm = withFormik({
         tos: tos || false,
       }
     },
+
+    //------- Validate Form --------
     validationSchema: Yup.object().shape({
       name: Yup.string()
         .required("Name is required.")
@@ -102,10 +125,16 @@ const FormikOnboardForm = withFormik({
       tos: Yup.boolean()
         .test("is-true", "You must agree to the terms to continue", value => value === true)
     }),
-    handleSubmit(values, {}) {
+
+    //-------- Submit form ----------
+    handleSubmit(values, {setStatus, resetForm}) {
       axios.post('https://reqres.in/api/users/', values)
-        .then(res => {console.log (res.data);})
-        .catch(err => console.log(err.response));
+        .then(res => {
+          console.log(res.data);
+          setStatus(res.data);
+          resetForm();
+        }) 
+        .catch(err => console.log(err.response))
     }
   })(OnboardForm)
 
